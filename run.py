936118138660b1d05bc6c71f6d5c05a5869ab9d0
Christoffer Henriksson <christoffer301@gmail.com>
2017-03-01 10:09:44 +0100

    clean up project structure

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ def main():
     sv_tags = ["<ROOT>", "ADJ", "ADP", "ADV", "AUX", "CONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB"]
     en_tags = ["<ROOT>", "ADJ", "ADP", "ADV", "AUX", "CONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X"]
 
-    sv_train_file = "sv_train.conllu"
-    sv_test_file = "sv_dev.conllu"
-    en_train_file = "en_train.conllu"
-    en_test_file = "en_dev.conllu"
+    sv_train_file = "treebanks/sv_train.conllu"
+    sv_test_file = "treebanks/sv_dev.conllu"
+    en_train_file = "treebanks/en_train.conllu"
+    en_test_file = "treebanks/en_dev.conllu"
     myTagger = tagger.Tagger(en_tags)
     myParser = parser.Parser(myTagger)
     dataReader.evaluate(en_train_file, en_test_file, myParser)
